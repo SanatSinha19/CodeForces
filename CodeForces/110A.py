f1593cd0a10938b69d5a7111fb7c8d4d9ba8974e
Sanat Sinha <itssanat196@gmail.com>
2025-06-14 20:49:39 +0530

    Added 116A

--- CodeForces/110A.py
+++ CodeForces/110A.py
@@ -4,7 +4,5 @@ def is_nearly_lucky(n):
         if digit == '4' or digit == '7':
             count += 1
     return "YES" if all(c in '47' for c in str(count)) else "NO"
-
-# Input
 n = input()
 print(is_nearly_lucky(n))
